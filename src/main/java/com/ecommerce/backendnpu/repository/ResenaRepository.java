b93aa981d73b5046387412bedb241aff9f1bfecc
ale <roa.alejandra02@gmail.com>
2025-04-14 13:35:02 -0400

    se arreglaron algunas lineas de codigo que estaban causando errores en la ejecucion del programa y tenian origen en ResenaRepository

--- src/main/java/com/ecommerce/backendnpu/repository/ResenaRepository.java
+++ src/main/java/com/ecommerce/backendnpu/repository/ResenaRepository.java
@@ -4,17 +4,20 @@ import com.ecommerce.backendnpu.model.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ResenaRepository extends JpaRepository<Resena, Long> {
-    // Métodos CRUD básicos están incluidos en JpaRepository
-    // Métodos personalizados (si los necesitas)
-    List<Resena> findByUsuarioId(Integer usuarioId);
 
-    List<Resena> findByProductosId(Integer productosId);
+    // Método para encontrar reseñas por puntaje
+    List<Resena> findByPuntaje(int puntaje);
 
-    // Puedes agregar más métodos de consulta personalizados si es necesario,
-    // por ejemplo, para filtrar por puntaje, rango de fechas, etc.
+    // Método para encontrar reseñas por producto (suponiendo que Resena tiene una relación con Producto)
+    List<Resena> findByProductoId(Long productoId);
 
+    // Método para obtener reseñas por rango de fechas
+    List<Resena> findByFechaBetween(LocalDate fechaInicio, LocalDate fechaFin);
 }
+
+
