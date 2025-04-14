8454c4403652d8ff3ad74f44ac530839594fc4af
ale <roa.alejandra02@gmail.com>
2025-04-14 13:11:21 -0400

    se agrego Pedido Controller a capa Api para probar su funcionalidad

---
+++ src/main/java/com/ecommerce/backendnpu/Api/PedidoRestController.java
@@ -0,0 +1,51 @@
+package com.ecommerce.backendnpu.Api;
+
+import com.ecommerce.backendnpu.model.Pedido;
+import com.ecommerce.backendnpu.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoRestController {
+    private  PedidoService pedidoService;
+
+    @Autowired
+    public PedidoRestController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    // Crear un nuevo pedido
+    @PostMapping
+    public ResponseEntity<Pedido> crearPedido(@RequestBody Pedido pedido) {
+        Pedido nuevoPedido = pedidoService.crearPedido(pedido);
+        return new ResponseEntity<>(nuevoPedido, HttpStatus.CREATED);
+    }
+
+    // Obtener todos los pedidos
+    @GetMapping
+    public ResponseEntity<List<Pedido>> obtenerTodosLosPedidos() {
+        List<Pedido> pedidos = pedidoService.obtenerTodosLosPedidos();
+        return new ResponseEntity<>(pedidos, HttpStatus.OK);
+    }
+
+
+
+    // Obtener un pedido por ID
+    @GetMapping("id/{id}")
+    public ResponseEntity<Pedido> obtenerPedidoPorId(@PathVariable Long id) {
+        Pedido pedido = pedidoService.obtenerPedidoPorId(id);
+        return new ResponseEntity<>(pedido, HttpStatus.OK);
+    }
+
+    // Eliminar un pedido por ID
+    @DeleteMapping("id/{id}")
+    public ResponseEntity<Void> eliminarPedido(@PathVariable Long id) {
+        pedidoService.eliminarPedido(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
