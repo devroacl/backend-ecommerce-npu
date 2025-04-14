ff98932b997e0121fd52744a82c308f502282378
ale <roa.alejandra02@gmail.com>
2025-04-14 13:41:22 -0400

    se modifico PedidoService por errores que causaba en la ejecucion del programa e inconsistencias con pedidoServiceImpl

--- src/main/java/com/ecommerce/backendnpu/service/PedidoService.java
+++ src/main/java/com/ecommerce/backendnpu/service/PedidoService.java
@@ -6,20 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PedidoService {
-    List<Pedido> findAllPedidos();
-
-    Optional<Pedido> findPedidoById(Long id);
-
-    List<Pedido> findPedidosByUsuarioId(Long usuarioId);
-
-    Pedido savePedido(Pedido pedido); // Para crear un nuevo pedido o actualizar uno existente
-
-    void deletePedidoById(Long id);
-
-    // Métodos adicionales específicos de la lógica de negocio del ecommerce:
-    Pedido crearNuevoPedido(Long usuarioId, List<Long> productoIdsConCantidades); // Ejemplo de método para crear un pedido
-
-    void actualizarEstadoPedido(Long pedidoId, String nuevoEstado);
-    // Otros métodos relacionados con la gestión de pedidos (cancelar, etc.)
+    Pedido crearPedido(Pedido pedido);
+    Pedido obtenerPedidoPorId(Long id);
+    List<Pedido> obtenerTodosLosPedidos();
+    void eliminarPedido(Long id);
 }
 
