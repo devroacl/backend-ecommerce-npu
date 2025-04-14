70f1762d3e653a62595afe6c1c3e6431ea55652d
ale <roa.alejandra02@gmail.com>
2025-04-14 15:37:55 -0400

    Se agrego ResenaService  y se modifico debido a que causaba inconsistencias con resenaseviceImpl

--- src/main/java/com/ecommerce/backendnpu/service/ResenaService.java
+++ src/main/java/com/ecommerce/backendnpu/service/ResenaService.java
@@ -1,25 +1,19 @@
 package com.ecommerce.backendnpu.service;
 
 import com.ecommerce.backendnpu.model.Resena;
+import com.ecommerce.backendnpu.repository.ResenaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ResenaService {
-    List<Resena> getAllResenas();
+    List<Resena> obtenerTodasLasResenas();
+    Optional<Resena> obtenerResenaPorId(Long id);
+    Resena guardarResena(Resena resena);
+    List<Resena> obtenerResenasPorPuntaje(int puntaje);
+    List<Resena> obtenerResenasPorProducto(Long productoId);
+    List<Resena> obtenerResenasPorFecha(LocalDate fechaInicio, LocalDate fechaFin);
 
-    Optional<Resena> getResenaById(Integer id);
-
-    Resena saveResena(Resena resena);
-
-    Resena updateResena(Resena resena);
-
-    void deleteResena(Integer id);
-
-    List<Resena> getResenasByUsuarioId(Integer usuarioId);
-
-    List<Resena> getResenasByProductosId(Integer productosId);
-
-    // Puedes agregar más métodos específicos de la lógica de negocio,
-    // por ejemplo, calcular el puntaje promedio de un producto.
 }
