522a2fb33ef162576026c095ca6e31851674e069
ale <roa.alejandra02@gmail.com>
2025-04-14 13:31:09 -0400

    se arreglaron inconsistencias en pedidoRepository que causaban errores en la ejecucion

--- src/main/java/com/ecommerce/backendnpu/repository/PedidoRepository.java
+++ src/main/java/com/ecommerce/backendnpu/repository/PedidoRepository.java
@@ -9,11 +9,6 @@ import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByUsuario_Id(Long usuarioId); // Obtener pedidos de un usuario específico
-
-    // Puedes agregar más métodos según las necesidades del negocio, por ejemplo:
-    List<Pedido> findByEstadoPedido_Nombre(String estado); // Filtrar pedidos por estado
-    List<Pedido> findByFechaPedidoBetween(LocalDateTime inicio, LocalDateTime fin); // Obtener pedidos en un rango de fechas
-
+    // Ya hereda métodos como save(), findById(), findAll(), deleteById()
 
 }
