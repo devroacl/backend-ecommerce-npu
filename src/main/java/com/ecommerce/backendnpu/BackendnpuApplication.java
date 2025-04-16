e21b73983045570176e2ea20958d0c4851c6d156
ale <roa.alejandra02@gmail.com>
2025-04-15 22:13:39 -0400

    se agregaron beans para poder mostrar en consola si la conexion a base de datos fue exitosa y el tiempo de despliegue de programa

--- src/main/java/com/ecommerce/backendnpu/BackendnpuApplication.java
+++ src/main/java/com/ecommerce/backendnpu/BackendnpuApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -16,15 +17,32 @@ public class BackendnpuApplication {
 	}
 
 	@Bean
-	CommandLineRunner checkDatabase(DataSource dataSource) {
+	CommandLineRunner checkDatabase(DataSource dataSource, Environment env) {
 		return args -> {
 			try {
-				System.out.println("🔍 URL de conexión REAL: " + dataSource.getConnection().getMetaData().getURL());
-				System.out.println("🔍 Usuario REAL: " + dataSource.getConnection().getMetaData().getUserName());
+				System.out.println("🔍 URL de conexión: " + dataSource.getConnection().getMetaData().getURL());
+				System.out.println("👤 Usuario de DB: " + dataSource.getConnection().getMetaData().getUserName());
+				System.out.println("🌱 Perfil activo: " + String.join(", ", env.getActiveProfiles()));
 			} catch (Exception e) {
 				System.err.println("❌ Error al obtener conexión: " + e.getMessage());
 			}
 		};
 	}
+
+
+
+
+	@Bean
+	CommandLineRunner measureStartupTime() {
+		return args -> {
+			long start = System.currentTimeMillis();
+			// Simula tareas de inicialización...
+			long end = System.currentTimeMillis();
+			System.out.println("⏱️ Tiempo de arranque aproximado: " + (end - start) + " ms");
+		};
+	}
+
+
+
 
 }
