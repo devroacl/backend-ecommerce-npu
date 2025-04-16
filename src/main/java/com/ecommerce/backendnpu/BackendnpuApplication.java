51d3cfa68326c7bfaf00322a4fa44956add01858
ale <roa.alejandra02@gmail.com>
2025-04-15 21:38:16 -0400

    se modifico backendapplication con la anotacion Bean para verificar credenciales con la base de datos local

--- src/main/java/com/ecommerce/backendnpu/BackendnpuApplication.java
+++ src/main/java/com/ecommerce/backendnpu/BackendnpuApplication.java
@@ -1,7 +1,11 @@
 package com.ecommerce.backendnpu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
@@ -9,6 +13,18 @@ public class BackendnpuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendnpuApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner checkDatabase(DataSource dataSource) {
+		return args -> {
+			try {
+				System.out.println("🔍 URL de conexión REAL: " + dataSource.getConnection().getMetaData().getURL());
+				System.out.println("🔍 Usuario REAL: " + dataSource.getConnection().getMetaData().getUserName());
+			} catch (Exception e) {
+				System.err.println("❌ Error al obtener conexión: " + e.getMessage());
+			}
+		};
 	}
 
 }
