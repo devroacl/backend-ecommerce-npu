02ecfd96170a991a6ad253668af2439a575a635f
ale <roa.alejandra02@gmail.com>
2025-04-18 00:51:43 -0400

    se modifico productorepository ya que tenia un metodo duplicado

--- src/main/java/com/ecommerce/backendnpu/repository/ProductoRepository.java
+++ src/main/java/com/ecommerce/backendnpu/repository/ProductoRepository.java
@@ -8,10 +8,6 @@ import java.util.List;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
-    List<Producto> findByNombreContaining(String nombre);
-    List<Producto> findByCategoriaId(Integer categoriaId);
-
     List<Producto> findByCategoriaId(Long categoriaId);
-
 
 }
