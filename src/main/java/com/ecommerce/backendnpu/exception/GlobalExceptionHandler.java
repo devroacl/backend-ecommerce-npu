605ec8475abc28470b83b7a30302fa264e29b413
ale <roa.alejandra02@gmail.com>
2025-04-20 20:17:41 -0400

    se agrega un globalexceptionHandler para manejar la mayoria de las excepciones que puedan ocurrir

--- src/main/java/com/ecommerce/backendnpu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/backendnpu/exception/GlobalExceptionHandler.java
@@ -1,35 +1,18 @@
 package com.ecommerce.backendnpu.exception;
-//No borrar es para maneja los errores de producto y que diga 404
+
+// Archivo: GlobalExceptionHandler.java
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException ex, WebRequest request) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("timestamp", new Date());
-        errorDetails.put("message", ex.getMessage());
-        errorDetails.put("details", request.getDescription(false));
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("timestamp", new Date());
-        errorDetails.put("message", ex.getMessage());
-        errorDetails.put("details", request.getDescription(false));
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(ProductoNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleProductoNotFoundException(ProductoNotFoundException e) {
+        return e.getMessage(); // Devuelve el mensaje de error con status 404
     }
 }
